e024973483ced0061ad0ba150f5ce51bcf4ae8e4
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-10-02 18:26:46 -0400

    fix(installer): proper login

--- installer/src/tasks/git_repositories.ts
+++ installer/src/tasks/git_repositories.ts
@@ -25,14 +25,6 @@ export const runGitRepositoriesTasks = async () => {
     [] as { cwd: string; name: string; folder?: string }[],
   );
 
-  await exec({
-    cmd: [
-      "gh",
-      "auth",
-      "login",
-    ],
-  });
-
   for await (const project of gitProjects) {
     const stat = await Deno.stat(project.cwd).catch(() => undefined);
     if (!stat) {
