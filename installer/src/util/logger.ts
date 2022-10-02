a56c889c205e8fc925b04729b81575463c0c94fb
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-10-02 18:47:57 -0400

    chore: add meta files (#3)

--- installer/src/util/logger.ts
+++ installer/src/util/logger.ts
@@ -1,15 +1,24 @@
-import { cyan, green } from "https://deno.land/std@0.158.0/fmt/colors.ts";
+import {
+  cyan,
+  green,
+  red,
+  yellow,
+} from "https://deno.land/std@0.158.0/fmt/colors.ts";
 
 export class Logger {
   public static info(message: string) {
-		console.log(`${cyan("[ Info ]")} ${message}`);
-	}
+    console.log(`${cyan("[ Info ]")} ${message}`);
+  }
 
   public static success(message: string) {
     console.log(`${green("[ Success ]")} ${message}`);
   }
 
-  public static warn(_message: string) {}
+  public static warn(message: string) {
+    console.log(`${yellow("[ Warning ]")} ${message}`);
+  }
 
-  public static error(_message: string) {}
+  public static error(message: string) {
+    console.log(`${red("[ Error ]")} ${message}`);
+  }
 }
