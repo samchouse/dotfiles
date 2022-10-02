4dd8ec7c42dc8d829cb44689574e5962908654bd
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-10-02 18:45:57 -0400

    test(installer): fix example config test

--- installer/src/util/config.ts
+++ installer/src/util/config.ts
@@ -1,43 +1,4 @@
-import * as z from "https://deno.land/x/zod@v3.19.1/mod.ts";
-
+import { configSchema } from "../schemas/mod.ts";
 import rawConfig from "../../config.json" assert { type: "json" };
-
-export const configSchema = z.object({
-  sudoPassword: z.string().optional(),
-  packageManagers: z.array(
-    z.object({
-      name: z.string(),
-      command: z.string(),
-      isSingleCommand: z.boolean().default(true),
-      args: z.array(z.string()),
-      packages: z.array(z.string()),
-      preInstall: z.array(z.string()).optional(),
-      postInstall: z.array(z.string()).optional(),
-    }),
-  ),
-  gitProjects: z.array(
-    z.object({
-      cwd: z.string(),
-      repositories: z.array(
-        z.string().or(z.object({
-          "name": z.string(),
-          "folder": z.string(),
-        })),
-      ),
-    }),
-  ),
-  copier: z.object({
-    configDir: z.string(),
-  }),
-}).refine((schema) =>
-  schema.packageManagers.reduce(
-        (prev, curr) => curr.command.includes("sudo") ? prev + 1 : prev,
-        0,
-      ) > 0 && !schema.sudoPassword
-    ? false
-    : true, {
-  message:
-    "sudoPassword is required when one of the package managers needs sudo to run",
-});
 
 export const config = configSchema.parse(rawConfig);
--- installer/tests/config_test.ts
+++ installer/tests/config_test.ts
@@ -1,6 +1,6 @@
 import { assertInstanceOf } from "https://deno.land/std@0.158.0/testing/asserts.ts";
 
-import { configSchema } from "../src/util/config.ts";
+import { configSchema } from "../src/schemas/mod.ts";
 import exampleConfig from "../config.example.json" assert { type: "json" };
 
 Deno.test("example config test", () => {
