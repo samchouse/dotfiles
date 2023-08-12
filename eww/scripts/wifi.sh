2b918b1ac1b6158a65ea1465ee0345fe9b543cbf
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-12 19:35:37 -0400

    feat(eww): use wlan1

--- eww/scripts/wifi.sh
+++ eww/scripts/wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-strength=$(iwctl station wlan0 show | grep -w RSSI | awk '{ print $2 }')
+strength=$(iwctl station wlan1 show | grep -w RSSI | awk '{ print $2 }')
 
 if [ "$strength" -ge -55 ]; then
 		echo ""
@@ -8,7 +8,7 @@ elif [ "$strength" -ge -63 ]; then
 		echo ""
 elif [ "$strength" -ge -70 ]; then
 		echo ""
-elif [ "$strength" -ge -75 ]; then
+elif [ "$strength" -ge -81 ]; then
 		echo ""
 else
 		echo ""
