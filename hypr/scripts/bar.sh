9ee1adcc353996943456b8cba812a54b7331d218
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-14 18:35:27 -0400

    fix(hypr): start slack minimized

--- hypr/scripts/bar.sh
+++ hypr/scripts/bar.sh
@@ -6,7 +6,7 @@ sleep 10
 flameshot &
 openrgb --startminimized --server &
 discord --start-minimized --enable-features=UseOzonePlatform --ozone-platform=wayland &
-slack --enable-features=UseOzonePlatform --enable-features=WebRTCPipeWireCapturer --enable-features=WaylandWindowDecorations --ozone-platform=wayland &
+slack --enable-features=UseOzonePlatform --enable-features=WebRTCPipeWireCapturer --enable-features=WaylandWindowDecorations --ozone-platform=wayland -u &
 
 sleep 3
 openrgb -p Blue &
