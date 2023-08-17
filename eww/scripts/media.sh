077474a76e17d0a5bd9c52531cdfba297465d2a8
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-16 22:25:03 -0400

    feat(eww): make the buttons functional

--- eww/scripts/media.sh
+++ eww/scripts/media.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
 case $1 in
-"metadata")
-  path=""
+"toggle")
+  playerctl --player playerctld play-pause
 
-  url=$(playerctl metadata mpris:artUrl)
-  if [[ $url == "https://"* ]]; then
-    ext="${url##*.}"
-
-    rm -rf ~/.config/eww/thumb.*
-    wget -q -O "$HOME/.config/eww/thumb.$ext" "$url"
-
-    path="$HOME/.config/eww/thumb.$ext"
-  elif [[ $url == "file://"* ]]; then
-    path="${url:7}"
+  isPlaying=$(~/.bin/eww get media_isplaying)
+  if [[ $isPlaying == "true" ]]; then
+    isPlaying="false"
+  else
+    isPlaying="true"
   fi
 
-  echo "{ \"title\": \"$(playerctl metadata xesam:title)\", \"path\": \"$path\" }"
+  ~/.bin/eww update "media_isplaying=$isPlaying"
+  exit
+  ;;
+esac
+
+case $2 in
+"metadata")
+  while getopts "sp" opt; do
+    case $opt in
+    s)
+      status=$(playerctl --player playerctld status)
+      if [[ $status == "Playing" ]]; then
+        echo "true"
+        exit
+      fi
+
+      echo "false"
+      ;;
+    p)
+      url=$(playerctl --player playerctld metadata mpris:artUrl)
+      if [[ $url == "https://"* ]]; then
+        ext="${url##*.}"
+
+        rm -rf ~/.config/eww/thumb.*
+        wget -q -O "$HOME/.config/eww/thumb.$ext" "$url"
+
+        echo "$HOME/.config/eww/thumb.$ext"
+      elif [[ $url == "file://"* ]]; then
+        echo "${url:7}"
+      fi
+      ;;
+    \?)
+      echo "Invalid option -$OPTARG" >&2
+      ;;
+    esac
+  done
   ;;
 *)
   echo "Invalid command"
