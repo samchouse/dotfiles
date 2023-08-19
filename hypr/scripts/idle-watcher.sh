bafdb014739430bd4c68f43518dbd039995b9cb5
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-08-19 19:38:30 -0400

    fix(hypr): sleep

--- hypr/scripts/idle-watcher.sh
+++ hypr/scripts/idle-watcher.sh
@@ -14,7 +14,7 @@ OPENRGB_ON_CMD="openrgb -p Blue"
 OPENRGB_OFF_CMD="openrgb -p Black"
 
 swayidle -w \
-  timeout 180 "$SWAYLOCK_CMD --grace 120" \
-  timeout 300 "$OPENRGB_OFF_CMD && hyprctl dispatch dpms off" resume "$OPENRGB_ON_CMD && hyprctl dispatch dpms on" \
-  before-sleep "$OPENRGB_OFF_CMD && $SWAYLOCK_CMD" \
+  timeout 180 "$OPENRGB_OFF_CMD && hyprctl dispatch dpms off" resume "$OPENRGB_ON_CMD && hyprctl dispatch dpms on" \
+  timeout 300 "hyprctl dispatch dpms on && systemctl suspend" \
+  before-sleep "$SWAYLOCK_CMD" \
   after-resume "$OPENRGB_ON_CMD"
