caf86ff23f023a5c0a080396f0a9e2aae9b1eb52
Samuel Corsi-House <chouse.samuel@gmail.com>
2023-10-01 17:30:11 -0400

    fix(hypr): switch out flameshot for watershot (#10)

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -3,7 +3,6 @@
 "$HOME/.bin/eww" open-many topbar-left topbar-right >> /dev/null 2>&1 &
 
 sleep 5
-flameshot >> /dev/null 2>&1 &
 openrgb --startminimized --server >> /dev/null 2>&1 &
 discord --start-minimized --enable-features=UseOzonePlatform --ozone-platform=wayland >> /dev/null 2>&1 &
 slack --enable-features=UseOzonePlatform --enable-features=WebRTCPipeWireCapturer --enable-features=WaylandWindowDecorations --ozone-platform=wayland -u >> /dev/null 2>&1 &
